bea9c3c8ae51bead70df869f3e2323021479dd45
JandersonConstantino <jandersonconstantino@gmail.com>
2019-03-29 13:37:01 -0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class App extends React.Component {
+  render = () => {
+    return (
+      <div>
+          <h2>Loot Check</h2>
+      </div>
+    )
+  }
+}
