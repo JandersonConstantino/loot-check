b17e553ae77f4154cca51a221cb614d3db347008
JandersonConstantino <jandersonconstantino@gmail.com>
2019-04-03 13:25:24 -0300

    buttons to deposit and withdraw

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,13 +1,39 @@
 import React from "react"
 import { connect } from "react-redux"
+import { deposit, withDraw } from  "../actions/balance"
 
 export class Wallet extends React.Component {
+  state = {
+    balance: undefined
+  }
+
+  updateBalance = e => this.setState({ balance: parseInt(e.target.value, 10) })
+
+  deposit = () => this.props.deposit(this.state.balance)
+
+  withDraw = () => this.props.withDraw(this.state.balance)
+
   render = () => {
     return (
       <div>
         <h3 className="balance">
           Wallet balance: {this.props.balance}
         </h3>
+        <br />
+        <input
+          className="input-wallet"
+          onChange={this.updateBalance}
+        />
+        <button
+          className="btn-deposit"
+          onClick={this.deposit}
+          children="Deposit"
+        />
+        <button
+          className="btn-withdraw"
+          onClick={this.withDraw}
+          children="Withdraw"
+        />
       </div>
     )
   }
@@ -17,4 +43,6 @@ const mapStateToProps = state => ({
   balance: state.balance
 })
 
-export default connect(mapStateToProps, null)(Wallet)
+export default connect(mapStateToProps, {
+  deposit, withDraw,
+})(Wallet)
--- src/components/__tests__/Wallet.test.js
+++ src/components/__tests__/Wallet.test.js
@@ -3,14 +3,64 @@ import { shallow } from "enzyme"
 import { Wallet } from "../Wallet"
 
 describe("Wallet", () => {
-  const props = { balance: 20 }
+  const mockDeposit = jest.fn()
+  const mockWithDraw = jest.fn()
+
+  const props = {
+    balance: 20,
+    deposit: mockDeposit,
+    withDraw: mockWithDraw
+  }
+  
   const wallet = shallow(<Wallet {...props} />)
 
   it("renders properly", () => {
     expect(wallet).toMatchSnapshot()
   })
 
+  describe("State init correctly", () => {
+    it("state `balance` init as undefined", () => {
+      expect(wallet.state().balance).toEqual(undefined)
+    })
+  })
+
   it("Display the balance from props", () => {
     expect(wallet.find(".balance").text()).toEqual("Wallet balance: 20")
+  })
+
+  it("creates an input to deposit into or withdraw from the balance", () => {
+    expect(wallet.find(".input-wallet").exists()).toBe(true)
+  })
+
+  describe("When the user types into the wallet input", () => {
+    const userBalance = "25"
+
+    beforeEach(() => {
+      wallet.find(".input-wallet").simulate("change", { target: { value: userBalance } })
+    })
+
+    it("updates the local wallet balance in `state` and converts it to a number", () => {
+      expect(wallet.state().balance).toEqual(parseInt(userBalance, 10))
+    })
+
+    describe("and the user wants to make a deposit", () => {
+      beforeEach(() => {
+        wallet.find(".btn-deposit").simulate("click")
+      })
+
+      it("dispatch the `deposit()` it receives from props with local balance", () => {
+        expect(mockDeposit).toHaveBeenCalledWith(parseInt(userBalance))
+      })
+    })
+
+    describe("and the user wants to make a withdraw", () => {
+      beforeEach(() => {
+        wallet.find(".btn-withdraw").simulate("click")
+      })
+
+      it("dispatch the `withDraw()` it receives from props with local balance", () => {
+        expect(mockWithDraw).toHaveBeenCalledWith(parseInt(userBalance))
+      })
+    })
   })
 })
