0d2988a375fbc171df6d41f986d0e9ba215b0484
JandersonConstantino <jandersonconstantino@gmail.com>
2019-04-08 21:31:18 -0300

    convert App.js to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,14 @@ import React from "react"
 import Wallet from "./Wallet"
 import Loot from "./Loot"
 
-export default class App extends React.Component {
-  render = () => {
-    return (
-      <div>
-          <h2>Loot Check</h2>
-          <hr />
-          <Wallet/>
-          <hr />
-          <Loot />
-      </div>
-    )
-  }
+export default () => {
+  return (
+    <div>
+      <h2>Loot Check</h2>
+      <hr />
+      <Wallet />
+      <hr />
+      <Loot />
+    </div>
+  )
 }
